cbfec497112f1db2c0e76986b53fc355ca3276ba
Ian McPhail <webguyian@gmail.com>
2024-02-03 16:16:44 -0500

    Update App styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import SearchFilter from './components/SearchFilter';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="flex flex-col h-screen text-[#111517] bg-[#fafafa] dark:text-white dark:bg-[#202c37]">
+      <Navbar />
+      <main className="flex flex-col h-full p-8 text-center">
+        <SearchFilter />
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </main>
     </div>
   );
 }
