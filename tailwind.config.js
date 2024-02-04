3269df1a7ce58b73e80c4b0c7d9b8c6cfff6ae82
Ian McPhail <webguyian@gmail.com>
2024-02-04 16:34:32 -0500

    Update configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {}
+    extend: {
+      gridTemplateColumns: {
+        fluid: 'repeat(auto-fit, minmax(250px, 1fr))'
+      }
+    }
   },
   plugins: []
 };
